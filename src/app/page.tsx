d61b92c50df334dadce02f03cd5d1ff1b9d00a37
Christine Amin <christine.t.amin@gmail.com>
2024-03-04 16:59:15 -0600

    started writing sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,51 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-function MyButton() {
+
+function SideBar() {
+  const links = [{
+    name: "about",
+    link: < a href="#about" > About</a >
+  }, {
+    name: "apply",
+    link: < a href="#apply" > Apply</a >,
+  }, {
+    name: "meet",
+    link: <a href="#meet-us">Meet Us</a>,
+  }, {
+    name: "donate",
+    link: <a href="#donate">Donate</a>,
+  }, {
+    name: "contact",
+    link: <a href = "#contact" > Contact</a >
+}];
+
+  const listLinks = links.map(link => <li key={link.name}>{link.link}</li>);
+
   return (
-    <button>I'm a button</button>
+    <>
+      <div className="side-bar">
+        <nav className="navigation">
+            <Image id="logo" src="images/logo.svg" alt="Logo" />
+          <ul>{listLinks}</ul>
+        </nav>
+    </div>
+    </>
   );
 }
 
-export default function Home() {
+function MainContent() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default function App() {
   return (
     <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
+      <SideBar />
+      <MainContent />
     </div>
 
   );
