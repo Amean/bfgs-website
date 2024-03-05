59d995d7934ba5ec7e56e59eef55372a521d8454
Christine Amin <christine.t.amin@gmail.com>
2024-03-05 15:31:30 -0600

    ported over images and implemented about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,27 +24,33 @@ function SideBar() {
 
   return (
     <>
-      <div className="side-bar">
+      {/* <div className="side-bar">
         <nav className="navigation">
             <Image id="logo" src="images/logo.svg" alt="Logo" />
           <ul>{listLinks}</ul>
         </nav>
-    </div>
+    </div> */}
     </>
   );
 }
 
 function About() {
   return (
-    <>
-    </>
+    <section id="about">
+      <Image src="/images/burbank.jpg" width={700} height={300} alt="Front of School" />
+      <h1>About Luther Burbank First Generation Scholarship</h1>
+      <p>This scholarship has been developed as an endowment by IB students from Burbank Class of 2001 as a
+          celebration of their 20th reunion. This $1,000 scholarship will be awarded to one student to provide financial support
+          and mentorship to first-generation college students. Successful applicants will be highly motivated with a strong desire to succeed and agreeable to participating in the financial literacy program this group of alumni intends to provide as well as the SAISD Foundation remote mentor program.
+      </p>
+    </section>
   );
 }
 
 function Apply() {
   return (
     <>
-    </>
+    </> 
   );
 }
 
@@ -71,13 +77,13 @@ function Contact() {
 
 function MainContent() {
   return (
-    <>
+    <div>
       <About />
       <Apply />
       <MeetUs />
       <Donate />
       <Contact />
-    </>
+    </div>
   );
 }
 
