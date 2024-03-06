1498c410145f1b33d9f7236e1c25015096b5629a
Christine Amin <christine.t.amin@gmail.com>
2024-03-06 14:23:06 -0600

    implemented apply section and started meet us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Image from 'next/image'
 import styles from './page.module.css'
 
@@ -47,17 +48,113 @@ function About() {
   );
 }
 
+function Button() {
+  // function handleClick() {
+  //   window.open('https://www.saisdfoundation.com/');
+  // }
+
+  return (
+    <button onClick={() => location.href = 'https://www.saisdfoundation.com/'}>
+      Apply
+    </button>
+  );
+}
+
 function Apply() {
   return (
-    <>
-    </> 
+    <section id="apply">
+      <h1>Who Can Apply</h1>
+      <p>Top 25% of graduating class from Burbank High School; minority; first-generation
+          college student
+          with the intent to attend a four-year college/university; and demonstrated need.</p>
+      <h1>How To Apply</h1>
+      <p>Applications are accepted through the link below</p>
+      <Button />
+    </section> 
   );
 }
 
 function MeetUs() {
   return (
-    <>
-    </>
+    <section id="meet-us">
+      <Image src="images/group_pic.jpg" width={300} height={300} alt="Group Picture" />
+      <h1>Past Winners</h1>
+      <div className="wrapper-winners">
+          <div className="cell">
+              <Image className="person" src="images/peyton_gamez.png" alt="Peyton Gamez" />
+              <h2>Peyton Gamez</h2>
+              <p>This cooll person won the scholarship for the class of blah blah blah. This person went on to do
+                  this thing all becuase
+                  of this scholarship. Fun cool facts about this person include how cool they are.</p>
+          </div>
+          <div className="cell">
+              <Image className="person" src="images/chris_mendez.png" alt="Chris Mendez" />
+              <h2>Chris Mendez</h2>
+              <p>This cooll person won the scholarship for the class of blah blah blah. This person went on to do
+                  this thing all becuase of this scholarship. Fun cool facts about this person include how cool
+                  they are.</p>
+          </div>
+
+      </div>
+      <h1>Scholarship Team</h1>
+      <div className="wrapper-team">
+          <div className="cell">
+              <Image className="person" src="images/peyton_gamez.png" alt="" />
+              <h2>Ana Sanchez</h2>
+              <p>Founder of Scholarship</p>
+          </div>
+          <div className="cell">
+              <Image className="person" src="images/chris_mendez.png" alt="" />
+              <h2>Liza Garcia</h2>
+              <p>Founder of Scholarship</p>
+          </div>
+          <div className="cell">
+              <Image className="person" src="images/peyton_gamez.png" alt="" />
+              <h2>Gregory Kitchen</h2>
+              <p>Founder of Scholarship</p>
+          </div>
+          <div className="cell">
+              <Image className="person" src="images/peyton_gamez.png" alt="" />
+              <h2>Enrique Loera</h2>
+              <p>Founder of Scholarship</p>
+          </div>
+          <div className="cell">
+              <Image className="person" src="images/chris_mendez.png" alt="" />
+              <h2>Vanessa Gonzalez</h2>
+              <p>Founder of Scholarship</p>
+          </div>
+          <div className="cell">
+              <Image className="person" src="images/peyton_gamez.png" alt="" />
+              <h2>Martin Vegara</h2>
+              <p>Founder of Scholarship</p>
+          </div>
+          <div className="cell">
+              <Image className="person" src="images/peyton_gamez.png" alt="" />
+              <h2>Ahmad Khazee</h2>
+              <p>Founder of Scholarship</p>
+          </div>
+          <div className="cell">
+              <Image className="person" src="images/chris_mendez.png" alt="" />
+              <h2>David Gomez</h2>
+              <p>Founder of Scholarship</p>
+          </div>
+          <div className="cell">
+              <Image className="person" src="images/peyton_gamez.png" alt="" />
+              <h2>Maria Mendoza-Reyna</h2>
+              <p>Founder of Scholarship</p>
+          </div>
+          <div className="cell">
+              <Image className="person" src="images/peyton_gamez.png" alt="" />
+              <h2>David DeLeon</h2>
+              <p>Founder of Scholarship</p>
+          </div>
+          <div className="cell">
+              <Image className="person" src="images/chris_mendez.png" alt="" />
+              <h2>Ralphie Baylor</h2>
+              <p>Founder of Scholarship</p>
+          </div>
+      </div>
+  </section>
   );
 }
 
